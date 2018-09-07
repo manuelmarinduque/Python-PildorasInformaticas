44c55eb7b275c46bc1e8a72c5c1f3d13a7a7a88d
ManuelMarinDuque <Manuel.Duque5.2@gmail.com>
2018-09-07 11:59:27 -0500

    Generadores

--- Generadores.py
+++ Generadores.py
@@ -2,7 +2,7 @@ def devuelve_ciudades(*ciudades):
     # * indica que se ingresa una cantidad indefinida de parámetros, los cuales
     # estarán dentro de una tupla.
     for elemento in ciudades:
-        yield elemento
+        yield from elemento
 
 # Creando el objeto generador:
 ciudades_devueltas = devuelve_ciudades("Madrid", "Barcelona", "Bilbao", "Valencia")
